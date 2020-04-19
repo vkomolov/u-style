a12958578955c1e6e2723df682230676f74c6073
vkomolov <vkomolov1974@gmail.com>
2020-04-19 08:50:39 +0300

    gulpfile initial settings

--- src/utils/pathMap.js
+++ src/utils/pathMap.js
@@ -5,28 +5,28 @@ module.exports = {
         html: './build/',
         js: './build/js/',
         style: './build/css/',
-        img: './build/img/',
+        img: './build/assets/img/',
         php: './build/php/'
     },
     src: {
         html: './src/*.html',
         js: './src/js/*.js',
         style: './src/scss/*.scss',
-        img: './src/img/**/*',
+        img: './src/assets/img/**/*',
         php: './src/php/*.php'
     },
     watch: {
         html: './src/*.html',
         js: './src/js/*.js',
         style: './src/scss/**/*.scss',
-        img: './src/img/**/*',
+        img: './src/assets/img/**/*',
         php: './src/php/**/*.php'
     },
     clean: {
         html: './build/*.html',
         style: './build/css/',
         js: './build/js/',
-        img: './build/img/*',
+        img: './build/assets/img/*',
         php: './build/php/*'
     }
 };
