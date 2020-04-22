5c6cf89390be9a106f8f34a0bf775f258644573f
vkomolov <vkomolov1974@gmail.com>
2020-04-23 01:18:31 +0300

    files for gh-pages

--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const funcs = require('./partial/funcs');
-
-console.log(funcs.test());
--- src/js/partial/funcs.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports = {
-    test: () => {
-        return "message from funcs.js";
-    }
-};
